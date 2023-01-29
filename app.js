997424e95f1ebf0205daba745849fd87b1d26bd3
mr-shresthi <akshresthi@gmail.com>
2023-01-29 11:59:14 +0530

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Tutorial by http://youtube.com/CodeExplained
-// api key : 82005d27a116c2880c8f0fcb866998a0
+
 
 // SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
@@ -9,9 +8,7 @@ const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
 
 // App data
-const weather = {
-    temperature : ""
-};
+const weather = {};
 
 weather.temperature = {
     unit : "celsius"
